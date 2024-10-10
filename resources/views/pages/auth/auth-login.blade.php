2a8cb92ceb42f5c7f7fc2ce43fe7d3dab4394a2e
rudialfa <rudih4236@gmail.com>
2024-10-10 11:11:56 +0700

    tinggal cara pakai fortify nya saja

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -14,8 +14,7 @@
         </div>
 
         <div class="card-body">
-            {{-- <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate=""> --}}
-                <form method="POST" action="" class="needs-validation" novalidate="">
+            <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                 @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
