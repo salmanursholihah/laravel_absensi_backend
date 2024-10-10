92e6e1c98e6c1f186c525e7896e7fcb18121a085
rudialfa <rudih4236@gmail.com>
2024-10-10 14:16:26 +0700

    store function

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -5,22 +5,45 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-       //index
-       public function index()
-       {
-           //search by name, pagination 10
-           $users = User::where('name', 'like', '%' . request('name') . '%')
-               ->orderBy('id', 'desc')
-               ->paginate(10);
-           return view('pages.users.index', compact('users'));
-       }
+    //index
+    public function index()
+    {
+        //search by name, pagination 10
+        $users = User::where('name', 'like', '%' . request('name') . '%')
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
 
-           //create
+    //create
     public function create()
     {
         return view('pages.users.create');
     }
+
+        //store
+        public function store(Request $request)
+        {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email',
+                'password' => 'required|min:8',
+            ]);
+    
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'role' => $request->role,
+                'password' => Hash::make($request->password),
+                // 'position' => $request->position,
+                // 'department' => $request->department,
+            ]);
+    
+            return redirect()->route('users.index')->with('success', 'User created successfully');
+        }
 }
