8a2a74b17a238a5470b6a9d4749f9bbd765016d2
salmanursholihah <salmanursholihah@gmail.com>
2025-06-24 11:45:11 +0700

    catatans

--- resources/views/pages/catatans/index.blade.php
+++ resources/views/pages/catatans/index.blade.php
@@ -12,6 +12,9 @@
     <section class="section">
         <div class="section-header">
             <h1>Catatans</h1>
+            <div class="section-header-button">
+                <a href="{{ route('catatan.create') }}" class="btn btn-primary">Add New</a>
+            </div>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="#">Catatans</a></div>
@@ -76,9 +79,9 @@
                                             <td>{{ $catatan->created_at }}</td>
                                             <td>{{ $catatan->updated_at }}</td>
                                             <td>
-                                                <a href="{{ route('catatans.edit', $catatan->id) }}"
+                                                <a href="{{ route('catatan.edit', $catatan->id) }}"
                                                     class="btn btn-sm btn-warning">Edit</a>
-                                                <form action="{{ route('catatans.destroy', $catatan->id) }}"
+                                                <form action="{{ route('catatan.destroy', $catatan->id) }}"
                                                     method="POST" style="display:inline;">
                                                     @csrf
                                                     @method('DELETE')
